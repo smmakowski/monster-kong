e29b308100bd2fb92a8d37980e6008eee2220e3b
smmakowski <makowski.sm@gmail.com>
2017-11-25 18:19:53 -0800

    load assets in PreloadState; add image files to directory

--- scripts/states/PreloadState.js
+++ scripts/states/PreloadState.js
@@ -5,7 +5,13 @@ let PreloadState ={
     this.loadingBar.anchor.setTo(.5); // center anchor to midddle of bar
     this.load.setPreloadSprite(this.loadingBar); // crops arg image depening on load percentage
     // preload assets here
-    this.load.image('background', '../../assets/images/sample-background.png');
+    this.load.image('actionButton', '../../assets/images/actionButton.png');
+    this.load.image('movementButton', '../../assets/images/movementButton.png');
+    this.load.image('barrel', '../../assets/images/barrel.png');
+    this.load.image('platform', '../../assets/images/platform.png');
+    this.load.image('ground', '../../assets/images/ground.png');
+    this.load.spriteSheet('fire', '../../assets/images/fire_spritsheet.png');
+    this.load.spriteSheet('player', '../../assets/images/player_spritesheet.png');
   },
   create: function() {
     console.log('Now in PreloadState');
