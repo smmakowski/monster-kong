0bf127f779acf214f11ecd806afd133208231f56
smmakowski <makowski.sm@gmail.com>
2017-11-25 18:41:09 -0800

    add gravity and restrict gravity/movement for basic sprites

--- scripts/states/BootState.js
+++ scripts/states/BootState.js
@@ -3,6 +3,10 @@ let BootState = {
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     this.scale.pageAlignVertically = true;
     this.scale.pageAlignHorizontally = true;
+
+    // add physics engine in boot
+    this.game.physics.startSystem(Phaser.Physics.ARCADE); // arcade phhyscis are not memory heave
+    this.game.physics.arcade.gravity.y = 1000; // set gravity (via trial and error) y means vertical x is horixonal
   },
   preload: function() {
     //preload assets for PreloadState
--- scripts/states/GameState.js
+++ scripts/states/GameState.js
@@ -1,16 +1,26 @@
 // game state object
 let GameState = {
   create: function() { // create scene here
+    const self = this;
   	// place background and add other sprites
     console.log('Now in GameState!');
     this.ground = this.add.sprite(0, 500, 'ground');
+    this.game.physics.arcade.enable(this.ground); //enable physics for entity
+    this.ground.body.allowGravity = false; // prevents gravity from affecting background
+    this.ground.body.immovable = true;
+    console.log(self.ground.body); // check out attributes of body in console
 
     let platform = this.add.sprite(0, 300, 'platform');
+    this.game.physics.arcade.enable(platform);
+    platform.body.allowGravity = false;
+    platform.body.immovable = true; // prevents movement due to colision
 
     this.player = this.add.sprite(100, 200, 'player', 3);
     this.player.anchor.setTo(.5);
     this.player.animations.add('walk', [0, 1, 2, 1], 6, true);
-    this.player.animations.play('walk');
+    this.game.physics.arcade.enable(this.player);
+
+    this.player.animations.play('walk'); // play animation
   },
   update: function() { // update methid
   	// update will run overthing under this method periodically during runtime
